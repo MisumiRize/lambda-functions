ae7c77d917b24c1f84ee395c624618ad4c464795
Misumi Rize <r@ayase-e.li>
2016-11-16 18:57:21 +0900

    make todoist-to-slack lambda ready

--- todoist-to-slack/index.js
+++ todoist-to-slack/index.js
@@ -1,37 +1,39 @@
-const co = require('co')
-const FormData = require('form-data')
-const moment = require('moment')
-const fetch = require('node-fetch')
+var co = require('co');
+var FormData = require('form-data');
+var moment = require('moment');
+var fetch = require('node-fetch');
 
-const body = new FormData()
-body.append('token', process.env.TODOIST_API_TOKEN)
-body.append('sync_token', '*')
-body.append('resource_types', '["items"]')
+var form = new FormData();
+form.append('token', process.env.TODOIST_API_TOKEN);
+form.append('sync_token', '*');
+form.append('resource_types', '["items"]');
 
-const today = moment()
+var today = moment();
 
-co(function *() {
-  const res = yield fetch('https://todoist.com/API/v7/sync', {method: 'post', body})
-  const json = yield res.json()
-  const items = json.items.filter(i => {
-    return moment(i['due_date_utc'], 'ddd DD MMM YYYY HH:mm:ss ZZ').isSame(today, 'day')
-  })
-
-  if (items.length == 0) {
-    return
-  }
-
-  yield fetch(process.env.SLACK_WEBHOOK_URL, {method: 'post', body: JSON.stringify({
-    username: 'todoist-bot',
-    icon_emoji: ':todoist:',
-    attachments: [
-      {
-        title: 'Todoist',
-        title_link: 'https://ja.todoist.com/',
-        pretext: `There are ${items.length} unfinished item(s) today`,
-        text: items.map(i => `* ${i.content}`).join('\n')
-      }
-    ]
-  })})
-}).then(() => console.log('ok'))
-  .catch(e => console.error(e))
+exports.handler = function(event, context, callback) {
+  co(function *() {
+    var res = yield fetch('https://todoist.com/API/v7/sync', {method: 'post', body: form});
+    var json = yield res.json();
+    var items = json.items.filter(function(i) {
+      return moment(i.due_date_utc, 'ddd DD MMM YYYY HH:mm:ss ZZ').isSame(today, 'day');
+    });
+  
+    if (items.length === 0) {
+      callback();
+    }
+  
+    yield fetch(process.env.SLACK_WEBHOOK_URL, {method: 'post', body: JSON.stringify({
+      username: 'todoist-bot',
+      icon_emoji: ':todoist:',
+      attachments: [
+        {
+          title: 'Todoist',
+          title_link: 'https://ja.todoist.com/',
+          pretext: 'There are ' + items.length + ' unfinished item(s) today',
+          text: items.map(function(i) { return '*' + i.content; }).join('\n')
+        }
+      ]
+    })});
+  }).then(function() { callback(); })
+    .catch(function(e) { callback(e); });
+};
