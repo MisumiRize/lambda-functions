6907e4cc911d5a7cd75ae8f8541e21a5aa1a9c41
Misumi Rize <r@ayase-e.li>
2016-11-19 14:06:56 +0900

    todoist-to-slack modified

--- todoist-to-slack/index.js
+++ todoist-to-slack/index.js
@@ -15,24 +15,18 @@ exports.handler = function(event, context, callback) {
     var res = yield fetch('https://todoist.com/API/v7/sync', {method: 'post', body: form});
     var json = yield res.json();
     var items = json.items.filter(function(i) {
-      return moment(i.due_date_utc, 'ddd DD MMM YYYY HH:mm:ss ZZ').isSame(today, 'day');
+      return moment(i.due_date_utc, 'ddd DD MMM YYYY HH:mm:ss ZZ').isSameOrBefore(today, 'day');
     });
   
     if (items.length === 0) {
-      callback();
+      return;
     }
   
     yield fetch(process.env.SLACK_WEBHOOK_URL, {method: 'post', body: JSON.stringify({
       username: 'todoist-bot',
       icon_emoji: ':todoist:',
-      attachments: [
-        {
-          title: 'Todoist',
-          title_link: 'https://ja.todoist.com/',
-          pretext: 'There are ' + items.length + ' unfinished item(s) today',
-          text: items.map(function(i) { return '*' + i.content; }).join('\n')
-        }
-      ]
+      text: 'There are ' + items.length + ' unfinished item(s) today at <Todoist>\n```'
+        + items.map(function(i) { return '*' + i.content; }).join('\n') + '\n```'
     })});
   }).then(function() { callback(); })
     .catch(function(e) { callback(e); });
